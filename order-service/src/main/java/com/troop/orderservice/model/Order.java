e1cfe22c214eb35443904ad03cf8e3521f142195
raja nabeel <raja.nabeel@tpltrakker.com>
2022-10-13 18:03:19 +0500

    order service repository initiated

---
+++ order-service/src/main/java/com/troop/orderservice/model/Order.java
@@ -0,0 +1,28 @@
+package com.troop.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+
+
+}
---
+++ order-service/src/main/java/com/troop/orderservice/service/OrderService.java
@@ -0,0 +1,34 @@
+package com.troop.orderservice.service;
+
+import com.troop.orderservice.dto.OrderLineItemsDto;
+import com.troop.orderservice.dto.OrderRequest;
+import com.troop.orderservice.model.Order;
+import com.troop.orderservice.model.OrderLineItems;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto).toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
